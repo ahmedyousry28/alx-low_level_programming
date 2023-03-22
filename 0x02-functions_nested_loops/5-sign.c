07c00b62ab2b4a271920e7579e2c928f6c69adad
ahmedyousry28 <ahmeddidamony9@gmail.com>
2023-03-22 08:12:19 -0700

    gteat

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_sign - this function check the nature of numbers
+ *
+ * @n: variable
+ *
+ * Return: 1 if positive , -1 if num is negative and return 0 if equl zero
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1)
+	else if (n < 0)
+		return (-1)
+	return (0)
+}
