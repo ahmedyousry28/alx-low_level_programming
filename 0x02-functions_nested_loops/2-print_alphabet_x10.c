50a896fb8f81a7d5f2d35b32a4cda32d4b826a15
ahmedyousry28 <ahmeddidamony9@gmail.com>
2023-03-22 05:41:45 -0700

    trying

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ * print_alphabet_x10
+ *
+ * Discription : a-z (10 times)
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int ch; 
+	int i;
+
+	for (i = 0; i < 10; i++)
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+	_putchar('\n');
+}
