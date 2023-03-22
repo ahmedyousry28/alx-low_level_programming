ce285e4d51116f0be741a5f26fe733a20f69a53e
ahmedyousry28 <ahmeddidamony9@gmail.com>
2023-03-22 13:25:32 -0700

    gteat

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,13 +4,10 @@
  *
  * @a: first number
  * @b: second number
- * @sum: summation of two variable
  *
  * Return: sum of two numbers
  */
 int add(int a, int b)
 {
-	int sum = a + b;
-
-	return (sum);
+	return (a + b);
 }
