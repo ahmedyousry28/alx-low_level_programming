fdf2811fbdba8fba5100cb3481f439564c537124
ahmedyousry28 <ahmeddidamony9@gmail.com>
2023-03-27 15:52:10 -0700

    gteat

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - to update the value to 98
+ *
+ * @*n: this is a decleration to pointer
+ *
+ * Return: always 0 (success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
